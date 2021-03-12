9d096237d1d30df70dede20089181107b9425bee
Aditya Kumarakrishnan <adityavkk@gmail.com>
2021-03-12 15:34:15 -0500

    init

---
+++ Probabalistic Data-Structures/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+module HashFunctions =
+    let SDBMHash (v:string) (m: int) : int =
+        let mutable h = 0
+        for i = 0 to v.Length - 1  do
+            h  <-  ((int)(v.Chars (i)))+(h <<< 6)+(h <<< 16)-h  
+        abs h % m
+
+    let SAXHash (v: string) (m: int): int =
+        let mutable h = 0
+        for i = 0 to v.Length - 1 do
+            h <-  h ^^^ (h <<< 5) + (h >>> 2) + (int)(v.Chars (i)) 
+        abs h % m
+
+
+[<EntryPoint>]
+let main argv =
+    let x = HashFunctions.SDBMHash ("foo") 12
+    let y = HashFunctions.SAXHash ("foobar") 12
+    printfn "SDBM Hash %A" x
+    printfn "SAX Hash %A" y
+    0 // return an integer exit code
